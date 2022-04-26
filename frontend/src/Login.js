6e16c95289225c023c1aedb354ecc8f07d3c44be
sarthakm21 <sarthakmaheshwari140@gmail.com>
2022-04-27 03:42:09 +0530

    button

---
+++ frontend/src/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  const handleClick = (e) => {};
+
+  return (
+    <div>
+      <button onClick={handleClick}>Login with MetaMask</button>
+    </div>
+  );
+};
+
+export default Login;
